6633f2de397b43c1fb2f1223c35b3693969dbcc1
Simon Serwazi <simonserwazi@gmail.com>
2023-06-05 16:35:27 +0200

    INIT

---
+++ Notenrechnerv2/Lehrer.java
@@ -0,0 +1,26 @@
+package Notenrechnerv2;
+import java.util.ArrayList;
+
+public class Lehrer {
+    //Fach 
+    //Schüler
+    //Test
+    private ArrayList<String> Fachliste=new ArrayList<String>();
+    private ArrayList<String> Schülerliste=new ArrayList<String>();
+    private ArrayList<String> Testliste=new ArrayList<String>();
+    public class Fach{
+        public String Fachname;
+
+    }
+    public class Schüler{
+        public String Schülername;
+
+
+    }
+    public class Test{
+
+    }
+    public void addFach(String Fachname){
+        Fach Fachname=new Fach();
+    }
+}
