8c1e7f309f7e4ca1339c4246041321fd1528f700
Simon Serwazi <simonserwazi@gmail.com>
2023-06-05 16:50:00 +0200

    Classen in eigene Dateien gesplittet

---
+++ Notenrechnerv2/Fach.java
@@ -0,0 +1,9 @@
+package Notenrechnerv2;
+
+public class Fach{
+    public String Fachname;
+    public Fach (String name){
+        Fachname=name;
+    }
+
+}
---
+++ Notenrechnerv2/Schüler.java
@@ -0,0 +1,10 @@
+package Notenrechnerv2;
+
+public class Schüler{
+    public String Schülername;
+    public Schüler(String name){
+        Schülername=name;
+    }
+
+
+}
