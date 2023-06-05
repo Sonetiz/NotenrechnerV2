8eec94f28b6a49318e6d40d429cf23c6e9e0e981
Simon Serwazi <simonserwazi@gmail.com>
2023-06-05 16:50:37 +0200

    classen gesplittet

--- Notenrechnerv2/Lehrer.java
+++ Notenrechnerv2/Lehrer.java
@@ -8,19 +8,13 @@ public class Lehrer {
     private ArrayList<String> Fachliste=new ArrayList<String>();
     private ArrayList<String> Schülerliste=new ArrayList<String>();
     private ArrayList<String> Testliste=new ArrayList<String>();
-    public class Fach{
-        public String Fachname;
+    public Lehrer(){
 
     }
-    public class Schüler{
-        public String Schülername;
-
-
-    }
-    public class Test{
-
-    }
+    
+    
+    
     public void addFach(String Fachname){
-        Fach Fachname=new Fach();
+        Fach myFach=new Fach(Fachname);
     }
 }
