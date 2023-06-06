1399b1b7803069ea4dcaca1626b789e64b98a6a9
simon <simonserwaz@gmail.com>
2023-06-06 19:47:56 +0200

    finished Test.java class

--- Notenrechnerv2/Test.java
+++ Notenrechnerv2/Test.java
@@ -1,5 +1,6 @@
 package Notenrechnerv2;
 
+import java.text.BreakIterator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,19 +34,50 @@ public class Test {
         }
         Testmap.remove(Schuelername);
     }
-    //addNote(Schueler,Note)
-    //editNote(Schueler,Note)
-    //getNote(Schuler)
-    //getTestaverage()
     //getSchuelers()
     public String[] getSchuelers(){
         if(Testmap.isEmpty()){
             return new String[0];
         }
         return Testmap.keySet().toArray(new String[Testmap.keySet().size()]);
-            
+        
         
     }
+    
+    //addNote(Schueler,Note)
+    public void addNote(String schuelerString,int Note){
+        if(!Testmap.containsKey(schuelerString)){
+            return;
+        }
+        Testmap.put(schuelerString,Note);
+    }
+    //editNote(Schueler,Note)
+    public void editNote(String SchuelerString,int newNote){
+        if(!Testmap.containsKey(SchuelerString)){
+            return;
+        }
+        Testmap.put(SchuelerString,newNote);
+    }
+    //getNote(Schuler)
+    public int getNote(String schuelerString){
+        if(!Testmap.containsKey(schuelerString)){
+            return-1;
+        }
+        return Testmap.get(schuelerString);
+    }
+    //getTestaverage()
+    public int getTestaverage(){
+        int anzahl=0;
+        int total=0;
+        for(String Schueler :Testmap.keySet()){
+            if(Testmap.get(Schueler)!=-1){
+                anzahl++;
+                total+=Testmap.get(Schueler);
 
+            }
+
+        }
+        return total/anzahl;
+    }
     
 }
