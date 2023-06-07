2c43476942143ffe68bc7faedd12be70773901ce
Simon Serwazi <simonserwazi@gmail.com>
2023-06-07 10:37:10 +0200

    implemented MSS->Notenconversion

--- Notenrechnerv2/Lehrer.java
+++ Notenrechnerv2/Lehrer.java
@@ -406,7 +406,81 @@ public class Lehrer {
     //initTest(Fach,Test,[Schueler])
     //getZeugnisnote(Fach,Schueler)
     //convertentry(1-6)
+    public Integer converttoMSS(Integer In){
+        if(In<1||In>6){
+            return -1;
+        }
+        switch(In){
+            case 1:
+                return 14;
+                
+            case 2:
+                return 11;
+                
+            case 3:
+                return 8;
+                
+            case 4:
+                return 5;
+                
+            case 5:
+                return 2;
+                
+            case 6:
+                return 0;
+                
+            default:
+                return -1;
+        }
+    }
     //convertexit(0-15)
+    public Integer convertfromMSS(Integer In){
+        if(In<0||In>15){
+            return -1;
+        }
+        switch(In){
+            case 0:
+                return 6;
+            case 1:
+                return 5;
+                
+            case 2:
+                return 5;
+                
+            case 3:
+                return 5;
+                
+            case 4:
+                return 4;
+                
+            case 5:
+                return 4;
+                
+            case 6:
+                return 4;
+                
+            case 7:
+                return 3;
+            case 8:
+                return 3;
+            case 9:
+                return 3;
+            case 10:
+                return 2;
+            case 11:
+                return 2;
+            case 12:
+                return 2;
+            case 13:
+                return 1;
+            case 14:
+                return 1;
+            case 15:
+                return 1;
+            default:
+                return -1;
+        }
+    }
     
     
 
